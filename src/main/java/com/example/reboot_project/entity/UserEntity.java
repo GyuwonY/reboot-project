0051d6a70b889790a085b1109848684cca2849d4
gwyoo <ykw3480@gmail.com>
2024-08-07 23:52:16 +0900

    init

---
+++ src/main/java/com/example/reboot_project/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package com.example.reboot_project.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+public class UserEntity extends BaseEntity {
+
+}
